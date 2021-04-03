7120d987cf83f80eb03f7450ab44d7076bd89e61
alejo4373 <alejandrofranco@pursuit.org>
2021-04-02 23:24:37 -0400

    add .gitignore and remove _site

--- _site/assets/js/handleForm.js
+++
@@ -1,6 +0,0 @@
-var form = document.querySelector(".contact-form");
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  var formData = new FormData(form);
-  window.location = `mailto:photobooth.snapandshare.help@gmail.com?subject=${formData.get('subject')}&body=${formData.get('body')}`;
-})
